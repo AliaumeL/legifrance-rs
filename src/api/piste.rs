9457dc0d708aa09c3dc7c0a46be879486de7f796
Aliaume LOPEZ <lopezaliaume@gmail.com>
2025-04-12 17:54:24 +0200

    Correct the search query!

--- src/api/piste.rs
+++ src/api/piste.rs
@@ -105,7 +105,7 @@ impl std::fmt::Display for Fond {
 /// 
 /// This should be sent as a JSON object in the body of a POST request
 /// to the authentication endpoint in the global variable `API_OAUTH`.
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct AuthBody {
     pub grant_type:    String,
     pub client_id:     String,
@@ -118,7 +118,7 @@ pub struct AuthBody {
 /// Note that typically the token is valid for 1 hour.
 /// To use this token, it suffices to place it in the header of the request
 /// to the API, in the `Authorization` field, prefixed by the string `Bearer `.
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct AuthResponse {
     pub access_token: String,
     pub token_type:   String,
@@ -129,22 +129,20 @@ pub struct AuthResponse {
 
 /// An object representing a search query.
 /// This is the object that is sent to the API on /search-like endpoints.
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct SearchQuery {
     #[serde(rename = "recherche")]
     pub search:      Search,
     pub fond:        String,
-    #[serde(rename = "filtres")]
-    pub filters:     Option<Vec<Filter>>,
-    pub sort:        Option<String>,
-    #[serde(rename = "secondSort")]
-    pub second_sort: Option<String>,
 }
 
 /// The object representing the search criteria.
 /// 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Search {
+    /// Whether the search is “advanced”, this is 
+    /// tied to the way the search bar of <legifrance.fr> is
+    /// built and should probably be set to false
     #[serde(rename = "fromAdvancedRecherche")]
     pub from_advanced: bool,
     /// Here we specify the type of search we want to do
@@ -166,11 +164,19 @@ pub struct Search {
     /// Page numbers should be between 1 and 100 inclusive.
     #[serde(rename = "pageNumber")]
     pub page_number: u8,
+    /// Filters to apply to the search.
+    #[serde(rename = "filtres")]
+    pub filters:     Option<Vec<Filter>>,
+    /// The sort order to use for the search.
+    pub sort:        Option<String>,
+    /// The second sort order to use for the search (in case of ties).
+    #[serde(rename = "secondSort")]
+    pub second_sort: Option<String>,
 }
 
 /// The type of pagination to use. 
 /// The behavior of `Article` is not yet documented.
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum Pagination {
     #[serde(rename = "DEFAUT")]
     Default,
@@ -178,7 +184,9 @@ pub enum Pagination {
     Article
 }
 
-#[derive(Serialize, Deserialize)]
+/// A logical operator for combining constraints
+/// can only be conjunction or disjunction.
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum Operator {
     #[serde(rename = "ET")]
     And,
@@ -186,14 +194,23 @@ pub enum Operator {
     Or,
 }
 
-#[derive(Serialize, Deserialize)]
+/// A filter to apply to the search. For now,
+/// only date filters are supported.
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Filter {
+    /// The date range to use as a filter.
     pub dates:   DateRange,
+    /// The field over which to apply the filter.
     pub facette: FilterType,
 }
 
-#[derive(Serialize, Deserialize)]
+/// The field over which to apply a given filter.
+/// For now, we only listed some 
+/// possibilities for date fields.
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum FilterType {
+    #[serde(rename = "DATE_DECISION")]
+    DecisionDate,
     #[serde(rename = "DATE_SIGNATURE")]
     SignatureDate,
     #[serde(rename = "DATE_PUBLICATION")]
@@ -204,14 +221,14 @@ pub enum FilterType {
 
 /// The date range to use for the search.
 /// Dates should be strings in the format `YYYY-MM-DD`.
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct DateRange {
     pub start: String,
     pub end:   String,
 }
 
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Field {
     #[serde(rename = "criteres")]
     pub constraints: Vec<Constraint>,
@@ -222,7 +239,7 @@ pub struct Field {
 }
 
 /// The criteria to use for the search.
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Constraint {
     /// The expected value (e.g. search term).
     #[serde(rename = "valeur")]
@@ -240,18 +257,18 @@ pub struct Constraint {
 /// The response from the API when searching.
 /// Note that we leave unspecified the `results` field
 /// for now.
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct SearchResponse {
     /// The total number of results that could match this query
     /// (note that this can be larger than the number of pages × page size,
     /// so some of them can be inaccessible).
     #[serde(rename = "totalResultNumber")]
     pub total_result_number: u64,
-    pub results: Vec<serde_json::Value>,
+    pub results: Vec<SearchResult>,
 }
 
 /// The type of search to use for a criteria.
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum MatchType {
     #[serde(rename = "UN_DES_MOTS")]
     OneOfTheWords,
@@ -265,9 +282,50 @@ pub enum MatchType {
     NoMatchToThisExpression,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 #[serde(rename_all = "UPPERCASE")]
 pub enum FieldType {
-    All
+    All,
+    Title,
+    Text,
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct SearchResult {
+    /// The “date utile” of the result.
+    /// (unclear what this means)
+    pub date: Option<String>,
+    /// The date of the result.
+    #[serde(rename = "datePublication")]
+    pub date_publication: Option<String>,
+    /// The date of the result's signature.
+    #[serde(rename = "dateSignature")]
+    pub date_signature: Option<String>,
+    /// The date of the result's diffusion.
+    #[serde(rename = "dateDiffusion")]
+    pub date_diffusion: Option<String>,
+    /// The array of “titles” of the result.
+    pub titles: Vec<SearchResultTitle>,
+    /// The text of the result.
+    pub text : Option<String>,
+    /// The current legal status of the result.
+    #[serde(rename = "etat")]
+    pub legal_status: Option<String>,
+    /// The origin of the result (dataset)
+    #[serde(rename = "origin")]
+    pub fond: Option<Fond>,
+    /// The type of document of the result.
+    #[serde(rename = "type")]
+    pub doctype: Option<String>,
+    pub nature: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct SearchResultTitle {
+    /// The title of the result.
+    pub title: String,
+    /// The id of the result.
+    pub id: String,
+    /// The chronological id of the result.
+    pub cid: String,
+}
